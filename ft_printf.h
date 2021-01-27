767bbdfb4c5866d8832ab906d63d161cd9a42f41
outSeop <53344972+outSeop@users.noreply.github.com>
2021-01-27 19:20:50 +0900

    test

--- ft_printf.h
+++ ft_printf.h
@@ -14,6 +14,7 @@
 int 		ft_printf(const char* , ...);
 int			check(const char *format);
 int			check_flag(const char *format);
+int			check_width(const char *format);
 int			check_type(const char *format);
 int			check_precision(const char *format);
 int			check_specifier(const char *format);
