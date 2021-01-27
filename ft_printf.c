ac4c68c3b0293eefc205f49e02c966ca2532428f
outSeop <53344972+outSeop@users.noreply.github.com>
2021-01-27 19:04:17 +0900

    test

--- ft_printf.c
+++ ft_printf.c
@@ -7,12 +7,17 @@ int			ft_printf(const char *format, ...)
 	int		i;
 
 	va_start(g_ap, format);
-	i = 0;
+	check(format);
+	va_end(g_ap);
+	return (1);
+}
+int			check(char *format)
+{
 	while (*format)
 	{
-		if (format[i] == '%')
+		if (*format == '%')
 		{
-		//	check_type(format);
+			check_type(format);
 		}
 		else
 		{
@@ -20,44 +25,59 @@ int			ft_printf(const char *format, ...)
 		}
 		format++;
 	}
-	return (1);
 }
-/*
-int			check_type(char *format, int *idx)
+int			check_type(char *format)
 {
-	return (check_flag(format, idx));
+	return (check_flag(format));
 }
 
-int			check_flag(char *foramt, int *idx)
+int			check_flag(char *foramt)
 {
-	if (format[0] == '-')
-		check_width(format, )
+	if (*format == '-')
+		check_width(++format)
 
-	else if (format[0] == '0')
-		check_width(format, ++(*idx))
+	else if (*format == '0')
+		check_width(++format)
 
 	else
-		check_width(format, idx);
-
+		check_width(format);
 }
 
-int			check_width(char *format, int *idx)
+int			check_width(char *format)
 {
 	int		width;
 	int		input_width;
 	char	*string;
 
-	if (format[0] == '*')
+	if (*format == '*')
 		width = va_arg(g_ap, int);
 	else
 	{
 		width = 0;
-		while (ft_isdisit(format[*idx]))
+		while (ft_isdigit(*format))
 		{
 			width *= 10;
-			width += ft_atoi(format[*idx]);
-			*idx++;
+			width += ft_atoi(*format);
+			format++;
 		}
 	}
+	check_precision(format, width);
 }
-*/
+
+int			check_precision(char *format, int width)
+{
+	int		precision;
+
+	precision = 0;
+	if (*format == '.')
+	{
+		format++;
+		while (ft_isdigit(*format))
+		{
+			precision *= 10;
+			precision += ft_atoi(*format);
+			format++;
+		}
+	}
+	check_specifier(format);
+}
