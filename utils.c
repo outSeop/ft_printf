597c7f34fded4dcfd8b76610d8cfebf5fc5fe391
outSeop <53344972+outSeop@users.noreply.github.com>
2021-02-05 19:22:00 +0900

    test

--- utils.c
+++ utils.c
@@ -29,6 +29,7 @@ char			*my_str_cpy(char *str, char c, int nbr)
 void			initTag(t_tag *tag)
 {
 	tag->arg_len = 0;
+	tag->prec_len = 0;
 	tag->argument = NULL;
 	tag->fill = '\0';
 	tag->sign = '\0';
