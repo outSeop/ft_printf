b251845f059e5fbb0872b3c732c60637913bded3
outSeop <53344972+outSeop@users.noreply.github.com>
2021-02-11 02:15:16 +0900

    test

--- format.c
+++ format.c
@@ -9,7 +9,7 @@ char			*algin(t_tag *tag)
 		if (tag->sign < 0 && tag->specifier == 'd')
 		{
 			if (tag->fill == '0')
-			return (insert_char(tag->argument, 1, tag->width, tag->fill));
+			return (insert_char(tag->argument, 1, tag->width + 1, tag->fill));
 		}
 	return (insert_char(tag->argument, 0, tag->width, tag->fill));
 	}
