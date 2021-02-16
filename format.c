05d26c60fe1723de6b1d798941ad183367616ba2
outSeop <53344972+outSeop@users.noreply.github.com>
2021-02-16 17:34:49 +0900

    test

--- format.c
+++ format.c
@@ -29,7 +29,7 @@ char			*refine(t_tag *tag)
 		if (tag->sign < 0 && (tag->specifier == 'd' || tag->specifier == 'i'))
 			return (insert_char(tag->argument, 1, tag->prec_len + 1, '0'));
 		if (tag->specifier == 'p')
-			return (insert_char(tag->argument, 2, tag->prec_len, '0'));
+			return (insert_char(tag->argument, 2, tag->prec_len + 2, '0'));
 		else
 			return (insert_char(tag->argument, 0, tag->prec_len, '0'));
 	}
