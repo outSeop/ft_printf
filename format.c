c46cb7e9a4195947fb25060d3dd7afb664d509e9
outSeop <53344972+outSeop@users.noreply.github.com>
2021-02-17 03:50:24 +0900

    test

--- format.c
+++ format.c
@@ -163,15 +163,15 @@ char			*insert_char(char *str, int idx, int size, char c)
 		return (NULL);
 	i = 0;
 	ft_strlcpy(result, str, idx);
-	printf("1: %s\n", result);
+	printf("\n1: [%s]\n", result);
 	while (i < size - str_len)
 	{
 		result[idx + i] = c;
 		i++;
 	}
-	printf("2: %s\n", result);
+	printf("2: [%s]\n", result);
 	ft_strlcpy(result + idx + i, str + idx, str_len - idx + 1);
-	printf("3: %s\n", result);
+	printf("3: [%s]\n", result);
 	result[size] = '\0';
 	free(str);
 	return (result);
